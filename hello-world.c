a318167c818462c0b856f80102b9f4674bcd19f4
Lucero Jiménez <93746778+pollitojeon@users.noreply.github.com>
2024-08-11 23:45:12 -0600

    GTK
    GTK

---
+++ hello-world.c
@@ -0,0 +1,115 @@
+
+**gcc pkg-config --cflags gtk+-3.0 hello-world.c -o helloworldEXE pkg-config --libs gtk+-3.0
+
+#include <gtk/gtk.h>
+
+
+
+void hello (GtkWidget *widget, gpointer data)
+
+{
+
+	g_print("Hello world\n");
+
+}
+
+
+gint delete_event (GtkWidget *widget,
+
+		GdkEvent *event,
+
+		gpointer data )
+
+
+{
+
+		g_print("delete event ocurred\n");
+
+
+		return TRUE;
+
+}
+
+
+void destroy (GtkWidget *widget, gpointer data)
+
+{
+
+	gtk_main_quit( );
+
+}
+
+
+
+
+
+int main (int argc, char *argv [ ])
+
+{
+
+	GtkWidget *window;
+
+	GtkWidget *button;
+
+
+
+	gtk_init(&argc, &argv);
+
+
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+
+
+	g_signal_connect(G_OBJECT (window), "delete_event" ,
+
+			G_CALLBACK (delete_event), NULL);
+
+
+
+	g_signal_connect(G_OBJECT (window), "destroy" ,
+
+			G_CALLBACK (destroy), NULL);
+
+
+
+	gtk_container_set_border_width (GTK_CONTAINER (window),10);
+
+
+
+	button = gtk_button_new_with_label ("LUCERO");
+
+
+
+	g_signal_connect(G_OBJECT (button), "clicked" ,
+
+			G_CALLBACK (hello), NULL);
+
+
+
+	g_signal_connect_swapped (G_OBJECT (button), "clicked" ,
+
+			G_CALLBACK (gtk_widget_destroy),
+
+			G_OBJECT (window));
+
+
+
+gtk_container_add(GTK_CONTAINER (window), button);
+
+
+
+gtk_widget_show (button);
+
+
+
+gtk_widget_show (window);
+
+
+
+gtk_main( ) ;
+
+
+
+}	
+
